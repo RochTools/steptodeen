b0a4b889a72e7b71323eca3306004e3a7fd94af4
StepTuDeen <hubproapk@gmail.com>
2026-08-28 05:10:47 +0500

    Adjust positioning and width in HomeView component

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -434,8 +434,8 @@ export const HomeView: React.FC<HomeViewProps> = ({
   className="pointer-events-none absolute z-[12] select-none opacity-90"
   style={{ 
     right: '21px',
-    top: '-15px',
-    width: '95%',
+    top: '-10px',
+    width: '94%',
     maxHeight: 'calc(100% - 90px)',
     objectFit: 'contain',
     objectPosition: 'top right'
